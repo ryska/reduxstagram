0344f9e937a6a44586a440b92b0c477953809a43
Adrianna Rys <adrianna.rys@xfive.co>
2017-02-17 15:32:41 +0100

    9/20

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        im a single
+      </div>
+    )
+  }
+})
+
+export default Single;
---
+++ client/reducers/posts.js
@@ -0,0 +1,8 @@
+// a reducer takes in 2 things - 1:action(infro about what happened) 2: copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
